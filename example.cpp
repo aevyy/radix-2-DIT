7352a2ff926aa1d3a2783d9ff7fbb07584f545a9
aevyy <wrk4avi@gmail.com>
2025-07-11 18:48:54 -0500

    Complete: radix-2 DIT FFT algorithm

--- example.cpp
+++ example.cpp
@@ -2,5 +2,16 @@
 #include "dft.hpp"
 
 int main() {
-    
+    dsp::fft myFFT;
+
+    std::vector<std::complex<double>> input = {
+        1, 2, 3, 4, 5   // Length 5, not a power of 2
+    };
+
+    auto output = myFFT.compute(input);
+
+    std::cout << "FFT Output:\n";
+    for (const auto &val : output) {
+        std::cout << val << "\n";
+    }
 }
---
+++ fft.cpp
@@ -0,0 +1,60 @@
+#include "fft.hpp"
+
+namespace dsp {
+
+bool fft::isPowerOfTwo(int n) {
+    return ((n > 0) && (n & n-1)) == 0;
+}
+
+int fft::nextPowerOfTwo(int n) {
+    if (n <= 0) return 1;
+    int p = 1;
+    while (p < n) p <<= 1;
+    return p;
+}
+
+std::vector<std::complex<double>> fft::compute(const std::vector<std::complex<double>> &input) {
+    int originalSize    = input.size();
+    int paddedSize      = nextPowerOfTwo(originalSize);
+
+    std::vector<std::complex<double>> paddedInput = input;
+    paddedInput.resize(paddedSize, 0.0);    // Padding with zerors
+
+    return recursiveFFT(paddedInput);
+
+}
+
+std::vector<std::complex<double>> fft::recursiveFFT(const std::vector<std::complex<double>> &x) {
+    int n = x.size();
+
+    // Base case
+    if (n == 1) return x;
+
+    // Seperating the input into ecen and odd indexed elements
+    std::vector<std::complex<double>> even(n/2), odd(n/2);
+    for (int i = 0; i < n/2; i++) {
+        even[i] = x[i * 2];     // -> x[0], x[2], ...
+        odd[i] = x[i * 2 + 1];  // -> x[1], x[2], ...
+    }
+
+    // Recursive calls to compute FFT of even and odd parts
+    auto E = recursiveFFT(even);    // E_k: FFT of even terms
+    auto O = recursiveFFT(odd);     // O_k: FFT of odd terms
+
+    // Now lets merge the odd and even results using twiddle factors
+    std::vector<std::complex<double>> X(n);
+    for (int k = 0; k < n/2; k++) {
+        // Twiddle factor: e^(-j * 2 * pi * k/n)
+        std::complex<double> twiddle = std::polar(1.0, -2 * M_PI * k / n);
+
+        // Butterfly operation:
+        // X{k] = E[k] + W * O[k]
+        // X[k + n/2] = E[k] - W * O[K]
+        X[k] = E[k] + twiddle * O[k];
+        X[k + n/2] = E[k] - twiddle * O[k];
+    }
+
+    return X;
+}
+
+}   // Namespace dsp
